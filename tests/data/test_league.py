828a6aa413ee527e65258625802e9cff0ecf1575
James Carter <tegatai@gmail.com>
2023-06-15 15:09:31 +0000

    League Class & Basic Tests (debugging in progress)

---
+++ tests/data/test_league.py
@@ -0,0 +1,79 @@
+"""Test League Module."""
+import pytest
+from iracing_web_api.data.league import League
+
+@pytest.fixture(scope="module")
+def league_instance(http_session):
+    """Return a League object."""
+    return League(http_session)
+
+def test_get_cust_league_sessions(league_instance):
+    """Test get_cust_league_sessions function."""
+    league_data = league_instance.get_cust_league_sessions(mine=True)
+    assert league_data
+    assert isinstance(league_data, dict)
+    assert league_data['success'] == True
+
+
+def test_get_directory(league_instance):
+    """Test get_directory function."""
+    league_data = league_instance.get_directory()
+    assert league_data
+    assert isinstance(league_data, dict)
+    assert league_data['success'] == True
+
+
+def test_get_league(league_instance):
+    """Test get_league function."""
+    league_data = league_instance.get_league(league_id=3580)
+    assert league_data
+    assert isinstance(league_data, dict)
+    assert league_data['success'] == True
+    assert league_data['league']['league_id'] == 3580
+
+
+def test_get_points_systems(league_instance):
+    """Test get_points_systems function."""
+    points_systems = league_instance.get_points_systems(league_id=3580)
+    assert points_systems
+    assert isinstance(points_systems, dict)
+    assert points_systems['success'] == True
+    assert points_systems['points_systems'][0]['league_id'] == 3580
+
+
+def test_get_membership(league_instance):
+    """Test get_membership function."""
+    membership = league_instance.get_membership(league_id=3580)
+    assert membership
+    assert isinstance(membership, dict)
+    assert membership['success'] == True
+    assert membership['membership'][0]['league_id'] == 3580
+
+
+def test_get_seasons(league_instance):
+    """Test get_seasons function."""
+    seasons = league_instance.get_seasons(league_id=3580)
+    assert seasons
+    assert isinstance(seasons, dict)
+    assert seasons['success'] == True
+    assert seasons['seasons'][0]['league_id'] == 3580
+
+def test_get_season_standings(league_instance):
+    """Test get_season_standings function."""
+    standings = league_instance.get_season_standings(league_id=3580, season_id=0)
+    assert standings
+    assert isinstance(standings, dict)
+    assert standings['success'] == True
+    assert standings['standings'][0]['league_id'] == 3580
+    assert standings['standings'][0]['season_id'] == 0
+
+
+def test_get_season_sessions(league_instance):
+    """Test get_season_sessions function."""
+    sessions = league_instance.get_season_sessions(league_id=3580, season_id=0)
+    assert sessions
+    assert isinstance(sessions, dict)
+    assert sessions['success'] == True
+    assert sessions['sessions'][0]['league_id'] == 3580
+    assert sessions['sessions'][0]['season_id'] == 0
+    
